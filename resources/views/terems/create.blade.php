47878123dfa2334e13b3045d2bd9f6c4f9977be0
HunSpino <tamasleventelaszlo99@gmail.com>
2022-02-22 11:23:43 +0100

    create.blade befejezve a termekhez

--- resources/views/terems/create.blade.php
+++ resources/views/terems/create.blade.php
@@ -6,7 +6,34 @@
 <h1>New PC</h1>
     <form method='POST' action="{{ route('terems.store') }}">
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
+        <div>
+            Épület: <br>
+            <input type="text" name="epulet" value="{{ old('epulet') }}">
+            @error('epulet')
+                <p>{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            Emelet: <br>
+            <input type="number" name="emelet" value="{{ old('emelet') }}">
+            @error('emelet')
+                <p>{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            Gép: <br>
+            <input type="number" name="gep" value="{{ old('gep') }}">
+            @error('gep')
+                <p>{{ $message }}</p>
+            @enderror
+        </div>
+        <div>
+            Emelet: <br>
+            <input type="number" name="emelet" value="{{ old('emelet') }}">
+            @error('emelet')
+                <p>{{ $message }}</p>
+            @enderror
+        </div>
         <div>
             <input type="submit" value="Create" class="btn btn-primary">
         </div>
