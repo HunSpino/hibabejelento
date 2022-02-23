ad62d9a67276c29f56f2e7af6838ef12eeafc900
HunSpino <tamasleventelaszlo99@gmail.com>
2022-02-23 14:44:37 +0100

    fillable and visible cuccok megvannak

--- app/Models/Hiba.php
+++ app/Models/Hiba.php
@@ -12,4 +12,8 @@ class Hiba extends Model
     public function terem() {
         return $this->belongsTo(Terem::class);
     }
+
+    protected $fillable = ['felhasznalo','hiba','terem_id'];
+
+    protected $visible = ['felhasznalo','hiba','terem_id'];
 }
--- app/Models/Terem.php
+++ app/Models/Terem.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Terem extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['epulet','emelet','terem','gep'];
+
+    protected $visible = ['epulet','emelet','terem','gep'];
 }
